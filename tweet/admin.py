2dcc4842f133832f32f1e8a56a6ac78b8c568fb9
Aravinda <aravinda.sholla@fullthrottlelabs.com>
2022-01-30 13:58:12 +0530

    Add APIs

--- tweet/admin.py
+++ tweet/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tweet, User
+
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "author",
+    )
+    list_display_links = ("id",)
+    search_fields = (
+        "id",
+        "text",
+    )
+    list_per_page = 50
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username")
+    list_display_links = ("id",)
+    search_fields = (
+        "id",
+        "username",
+    )
+    list_per_page = 50
--- tweet/views.py
+++ tweet/views.py
@@ -1,6 +1,10 @@
-from rest_framework import mixins, permissions, viewsets
+from rest_framework import mixins, permissions, status, serializers, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import User, Tweet
 from .serializers import UserSerializer, TweetSerializer
+from .utils import CustomAPIException
 
 
 class UserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -12,9 +16,73 @@ class TweetViewSet(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
-    mixins.DestroyModelMixin,
     viewsets.GenericViewSet,
 ):
     queryset = Tweet.objects.all()
     serializer_class = TweetSerializer
     permission_classes = (permissions.IsAuthenticated,)
+
+    class RecordsSerializer(serializers.Serializer):
+        """Ids serializer."""
+
+        ids = serializers.ListField(child=serializers.CharField(min_length=1))
+
+    class DateSerializer(serializers.Serializer):
+        """Date Serializer."""
+
+        date = serializers.DateField()
+
+    def get_queryset(self):
+        """Filter tweets of a user."""
+        queryset = self.queryset.filter(author=self.request.user)
+
+        if self.action in ["list"]:
+            if "date" not in self.request.query_params:
+                raise CustomAPIException(
+                    message="date query param is required.",
+                    status_code=status.HTTP_403_FORBIDDEN,
+                )
+
+        # Get the date query parameter
+        date_data = self.request.query_params.get("date")
+
+        # Valid if it is a date
+        date_serializer = self.DateSerializer(data={"date": date_data})
+        date_serializer.is_valid(raise_exception=True)
+
+        # Filter only newer according to the date
+        validated_date_data = date_serializer.validated_data["date"]
+        queryset = queryset.filter(created_at__date__gte=validated_date_data)
+
+        return queryset
+
+    def perform_create(self, serializer):
+        # Saving the author of the tweet
+        serializer.save(author=self.request.user)
+
+    @action(methods=["POST"], detail=False, url_path="multi/delete")
+    def multi_delete(self, request, *args, **kwargs):
+        """Delete multiple tweet records."""
+
+        # Verify ids
+        serializer = self.RecordsSerializer(
+            data={
+                "ids": request.data.get("ids"),
+            }
+        )
+        serializer.is_valid(raise_exception=True)
+
+        # Filter the tweets according to ids and author
+        tweet_ids = serializer.validated_data.get("ids")
+        queryset = self.queryset.filter(
+            id__in=tweet_ids, author=self.request.user
+        )
+
+        response = {
+            "count": queryset.count(),
+            "tweets": self.get_serializer(queryset, many=True).data,
+        }
+
+        # delete tweets
+        queryset.delete()
+        return Response(response)
